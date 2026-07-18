a4d701b23ae2f6675234447665d26591cfb89d71
Lukas <46909538+TanConan@users.noreply.github.com>
2026-07-18 22:34:51 +0200

    Add scss support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
-import App from "./App.vue";
-import router from "./router";
+import App from "@/App.vue";
+import router from "@/router";
+
+import "@/assets/styles/main.scss";
 
 const app = createApp(App);
 
